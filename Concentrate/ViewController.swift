a06516f3f34bdee21b1c2392af38d3081627532f
OmranK <omrankhoja@omrans-mbp.home>
2018-06-28 01:33:13 -0400

    Initial Commit

---
+++ Concentrate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentrate
+//
+//  Created by Omran Khoja on 6/28/18.
+//  Copyright © 2018 AcronDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
